ace2986d4b78488b42110a82d21d63ba8255e537
Sergi Mansilla <sergi.mansilla@gmail.com>
2013-11-06 17:03:16 +0100

    Settings UI ready
    Note - removed Persona UI from the settings app, rather than leave it
    alongside the FXA UI. Merge was otherwise straightforward.
    
    Conflicts:
    	apps/settings/index.html
    	apps/settings/style/settings.css

---
+++ apps/settings/js/fxa.js
@@ -0,0 +1,8 @@
+/* -*- Mode: js; js-indent-level: 2; indent-tabs-mode: nil -*- */
+/* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
+
+'use strict';
+
+var Accounts = (function account_settings() {
+
+})();
