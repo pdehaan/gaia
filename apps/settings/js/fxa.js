0b66d9066f27e8843e855d4d94771849352486b8
Jared Hirsch <ohai@6a68.net>
2014-01-16 15:58:14 -0800

    Playing with code structure + learning from other settings apps.
      Have the UI bits split into separate files, but don't have everything
      wired up quite yet. Good times thoguh.

--- apps/settings/js/fxa.js
+++ apps/settings/js/fxa.js
@@ -1,38 +1,26 @@
-// TODO what is user info format?
-// TODO do we need transitions for moving between logged in/out screens?
-// TODO do we need a third screen for "unverified/check your email"?
+/**
+ * Model code - fetches and holds state, updates the panel/menu as needed.
+ */
+
+// TODO Assuming getAccounts() responses are of the form
+//      user: { id: string, verified: boolean, email: email }
+//      there might be a sessionToken, but I don't need it I think.
+//      --> it is not clear what the logged out response looks like.
 // TODO do we want to disable some buttons after clicking?
 // TODO l10n!
 
 'use strict';
 
-var Accounts = (function account_settings() {
-
-  var loggedOutPanel,
-    loggedInPanel,
-    unverifiedPanel,
-    overlayPanel,
-    loginBtn,
-    logoutBtn,
-    deleteAccountBtn,
-    loggedInUser,
-    currentState = 'unknown',
-    currentUser;
+var Fxa = (function fxa() {
+    var currentState = 'unknown',
+      currentEmail;
 
   function init() {
-    loggedOutPanel = document.getElementById('fxa-logged-out');
-    loggedInPanel = document.getElementById('fxa-logged-in');
-    unverifiedPanel = document.getElementById('fxa-unverified');
-    overlayPanel = document.getElementById('fxa-overlay');
-    loginBtn = document.getElementById('fxa-login');
-    logoutBtn = document.getElementById('fxa-logout');
-    deleteAccountBtn = document.getElementById('fxa-delete-account');
-    loggedInEmail = document.getElementById('fxa-logged-in-email');
-
     FxAccountsIACHelper.getAccounts(onFxAccountStateChange, onFxAccountError);
     FxAccountsIACHelper.addEventListener('onlogin', refreshState);
     FxAccountsIACHelper.addEventListener('onverifiedlogin', refreshState);
     FxAccountsIACHelper.addEventListener('onlogout', refreshState);
+    // TODO need to remove listeners on document.hidden?
   }
 
   function refreshState() {
@@ -40,92 +28,55 @@ var Accounts = (function account_settings() {
     FxAccountsIACHelper.getAccounts(onFxAccountStateChange, onFxAccountError);
   }
 
-  // TODO guessing data format is { user: 'foo@bar.com', state: 'verified' }
-  // TODO guessing states are 'verified', 'unverified', 'loggedout', and null,
-  //      which we treat as 'unknown'
   function onFxAccountStateChange(data) {
-    // bail if the state is unchanged
-    if (!data && currentState == 'unknown' ||
-        currentState == data.state && currentUser == data.user) {
-      return;
-    }
-
-    // else assign it and update the DOM
-    currentUser = data && data.user;
-    currentState = data && data.state || 'unknown';
-
-    if (currentState == 'verified') {
-      showSpinner();
-      showLoggedInPanel();
-      hideLoggedOutPanel();
-      hideUnverifiedPanel();
-    } else if (currentState == 'unverified') {
-      showSpinner();
-      showUnverifiedPanel();
-      hideLoggedOutPanel();
-      hideLoggedInPanel();
-    } else if (currentState == 'loggedout') {
-      showSpinner();
-      showLoggedOutPanel();
-      hideLoggedInPanel();
-      hideUnverifiedPanel();
+    var state, email;
+    if (!data) {
+      state = 'unknown';
+      email = null;
+    } else if ('verified' in data) {
+      state = data.verified ? 'verified' : 'unverified';
+      email = data.email;
     } else {
-      // TODO accountState == 'unknown', display some TBD interstitial state
+      state = 'loggedout';
+      email = null;
     }
+
+    if (currentState != state || currentEmail != email) {
+      currentState = state;
+      currentEmail = email;
+      updateViews(state, email);
+    }
+  }
+
+  function updateViews(state, email) {
+    var event = new CustomEvent('fxaccountchange', {
+      state: state,
+      email: email
+    });
+    window.dispatchEvent(event);
   }
 
   function onFxAccountError(err) {
-    // TODO log error? retry?
+    console.error(err.msg);
+    // TODO maybe show overlay with error?
   }
 
-  function hideLoggedOutPanel() {
-    loginBtn.onclick = null;
-    loggedOutPanel.hidden = true;
+  /* Hiding the IAC helper from the views. TODO too much abstraction? */
+  function onLogout(e) {
+    FxAccountsIACHelper.logout(onFxAccountStateChange, onFxAccountError);
   }
 
-  function showLoggedOutPanel() {
-    loginBtn.onclick = FxAccountsIACHelper.openFlow(
-      onFxAccountStateChange, onFxAccountError);
-    loggedOutPanel.hidden = false;
-  }
-
-  function hideLoggedInPanel() {
-    loggedInPanel.hidden = true;
-    loggedInEmail.textContent = '';
-    logoutBtn.onclick = null;
-    deleteAccountBtn.onclick = null;
-  }
-
-  function showLoggedInPanel() {
-    // TODO how to escape this text?
-    loggedInEmail.textContent = currentUser;
-    loggedInPanel.hidden = false;
-    logoutBtn.onclick = FxAccountsIACHelper.logout(
-      onFxAccountStateChange, onFxAccountError);
-    // TODO insert bug number tracking this feature
-    deleteAccountBtn.onclick = alert('delete not yet implemented');
-  }
-
-  function hideUnverifiedPanel() {
-    unverifiedPanel.hidden = true;
-    // TODO wire up other elements
-  }
-
-  function showUnverifiedPanel() {
-    unverifiedPanel.hidden = false;
-    // TODO wire up other elements
-  }
-
-  function showSpinner() {
-    overlayPanel.hidden = false;
-    setTimeout(function() { overlayPanel.hidden = true }, 2000);
+  function onLogin(e) {
+    FxAccountsIACHelper.openFlow(onFxAccountStateChange, onFxAccountError);
   }
 
   // TODO need to return anything else?
   return {
-    init: init
+    init: init,
+    onLogout: onLogout,
+    onLogin: onLogin
   };
-
 })();
 
-navigator.mozL10n.ready(Accounts.init());
+// TODO init on 'panelready', not before.
+navigator.mozL10n.ready(Fxa.init());
--- apps/settings/js/fxa_menu.js
+++ apps/settings/js/fxa_menu.js
@@ -1,63 +1,46 @@
 
 /**
- * This displays the firefox accounts status in the main panel without
- * loading all the code in the main fxa.js file
+ * UI code for the firefox accounts menu entry in the settings panel
  */
+// TODO how do we get the initial state from the model?
 
-// TODO do we want to export anything here, so that the main panel can
-// communicate with this panel? Or maybe we just route all signals through the
-// FxAccountsIACHelper?
-var FxaMenu = {
-  currentState: 'unknown',
-  currentUser: undefined,
-  init: function init() {
+var FxaMenu = (function fxa_menu() {
+  function init() {
     var _ = navigator.mozL10n.get;
     var fxaMenuDesc = document.getElementById('fxa-desc'),
-    // get current state and update DOM
-    FxaMenu.refreshState();
 
-    // then, observe FxAccountsIACHelper for login, verified, logout events
-    // TODO do we need to detach listeners when the settings app is closed?
-    FxAccountsIACHelper.addEventListener('onlogin', FxaMenu.refreshState);
-    FxAccountsIACHelper.addEventListener('onverifiedlogin',
-      FxaMenu.refreshState);
-    FxAccountsIACHelper.addEventListener('onlogout', FxaMenu.refreshState);
-  },
+    onVisibilityChange();
+  }
 
-  refreshState: function refreshState() {
-    FxAccountsIACHelper.getAccounts(FxaMenu.onFxAccountsData,
-      FxaMenu.onFxAccountsError);
-  },
-
-  // TODO guessing data format is { user: 'foo@bar.com', state: 'verified' }
-  // TODO guessing states are 'verified', 'unverified', 'loggedout', and null,
-  //      which we treat as 'unknown'
-  onFxAccountsData: function onFxAccountsData(data) {
-    // bail if the state is unchanged
-    if (!data && currentState == 'unknown' ||
-        currentState == data.state && currentUser == data.user) {
-      return;
-    }
-
-    // else assign it and update the DOM
-    FxaMenu.currentUser = data && data.user;
-    FxaMenu.currentState = data && data.state || 'unknown';
-
-    if (FxaMenu.currentState == 'verified') {
-      fxaMenuDesc.text = _('Logged in as ') + FxaMenu.currentUser;
-    } else if (FxaMenu.currentState == 'unverified') {
+  function onFxaChange(data) {
+    var email = data.email,
+      state = data.state;
+    if (state == 'verified') {
+      // TODO escape email
+      fxaMenuDesc.text = _('Logged in as ') + email;
+    } else if (state == 'unverified') {
       fxaMenuDesc.text = _('Please check your email');
-    } else if (FxaMenu.currentState == 'loggedout') {
+    } else if (state == 'loggedout') {
       fxaMenuDesc.text = _('Log in to access your acct.');
     } else {
-      // accountState == 'unknown', display nothing
+      // state == 'unknown', display nothing
       fxaMenuDesc.text = '';
     }
-  },
-  onNetworkError: function onUpdateError(err) {
-    // TODO log error? retry?
   }
-};
+
+  function onVisibilityChange(e) {
+    if (document.hidden) {
+      window.removeEventListener('fxaccountchange', onFxaChange);
+      document.removeEventListener('visibilitychange', onVisibilityChange);
+    } else {
+      window.addEventListener('fxaccountchange', onFxaChange);
+      document.addEventListener('visibilitychange', onVisibilityChange);
+    }
+  }
+
+  // TODO return things & stuff
+  return {};
+});
 
 // starting when we get a chance
 navigator.mozL10n.ready(function loadWhenIdle() {
