c22985c71f3bb4334598960dbc4726ee265e612d
Jared Hirsch <ohai@6a68.net>
2014-01-16 16:21:07 -0800

    move everything into one js file

--- apps/settings/js/fxa.js
+++ apps/settings/js/fxa.js
@@ -78,5 +78,172 @@ var Fxa = (function fxa() {
   };
 })();
 
-// TODO init on 'panelready', not before.
 navigator.mozL10n.ready(Fxa.init());
+
+
+
+
+
+/**
+ * UI code for the firefox accounts menu entry in the settings panel
+ */
+// TODO how do we get the initial state from the model?
+
+var FxaMenu = (function fxa_menu() {
+  function init() {
+    var _ = navigator.mozL10n.get;
+    var fxaMenuDesc = document.getElementById('fxa-desc'),
+
+    onVisibilityChange();
+  }
+
+  function onFxaChange(data) {
+    var email = data.email,
+      state = data.state;
+    if (state == 'verified') {
+      // TODO escape email
+      fxaMenuDesc.text = _('Logged in as ') + email;
+    } else if (state == 'unverified') {
+      fxaMenuDesc.text = _('Please check your email');
+    } else if (state == 'loggedout') {
+      fxaMenuDesc.text = _('Log in to access your acct.');
+    } else {
+      // state == 'unknown', display nothing
+      fxaMenuDesc.text = '';
+    }
+  }
+
+  function onVisibilityChange(e) {
+    if (document.hidden) {
+      window.removeEventListener('fxaccountchange', onFxaChange);
+      document.removeEventListener('visibilitychange', onVisibilityChange);
+    } else {
+      window.addEventListener('fxaccountchange', onFxaChange);
+      document.addEventListener('visibilitychange', onVisibilityChange);
+    }
+  }
+
+  // TODO return things & stuff
+  return {};
+});
+
+// starting when we get a chance
+navigator.mozL10n.ready(function loadWhenIdle() {
+  var idleObserver = {
+    time: 5,
+    onidle: function() {
+      FxaMenu.init();
+      navigator.removeIdleObserver(idleObserver);
+    }
+  };
+  navigator.addIdleObserver(idleObserver);
+});
+
+
+
+/**
+ * UI code for the firefox accounts panel
+ *
+ */
+// TODO do we want to disable some buttons after clicking?
+// TODO how do we get initial state from the model?
+
+'use strict';
+
+var FxaPanel = (function fxa_panel() {
+  var loggedOutPanel,
+    loggedInPanel,
+    unverifiedPanel,
+    overlayPanel,
+    loginBtn,
+    logoutBtn,
+    deleteAccountBtn,
+    isVisible;
+
+  function init() {
+    loggedOutPanel = document.getElementById('fxa-logged-out');
+    loggedInPanel = document.getElementById('fxa-logged-in');
+    unverifiedPanel = document.getElementById('fxa-unverified');
+    overlayPanel = document.getElementById('fxa-overlay');
+    loginBtn = document.getElementById('fxa-login');
+    logoutBtn = document.getElementById('fxa-logout');
+    deleteAccountBtn = document.getElementById('fxa-delete-account');
+    loggedInEmail = document.getElementById('fxa-logged-in-email');
+
+    window.addEventListener('fxaccountchange', onFxaChange);
+  }
+
+  // TODO if not visible, bail.
+  function onFxaChange(e) {
+    var currentState = e.state,
+      currentEmail = e.email;
+
+    if (currentState == 'verified') {
+      showSpinner();
+      showLoggedInPanel();
+      hideLoggedOutPanel();
+      hideUnverifiedPanel();
+    } else if (currentState == 'unverified') {
+      showSpinner();
+      showUnverifiedPanel();
+      hideLoggedOutPanel();
+      hideLoggedInPanel();
+    } else if (currentState == 'loggedout') {
+      showSpinner();
+      showLoggedOutPanel();
+      hideLoggedInPanel();
+      hideUnverifiedPanel();
+    } else {
+      // TODO accountState == 'unknown', display some TBD interstitial state
+    }
+  }
+
+  function hideLoggedOutPanel() {
+    loginBtn.onclick = null;
+    loggedOutPanel.hidden = true;
+  }
+
+  function showLoggedOutPanel() {
+    loginBtn.onclick = Fxa.onLogin;
+    loggedOutPanel.hidden = false;
+  }
+
+  function hideLoggedInPanel() {
+    loggedInPanel.hidden = true;
+    loggedInEmail.textContent = '';
+    logoutBtn.onclick = null;
+    deleteAccountBtn.onclick = null;
+  }
+
+  function showLoggedInPanel() {
+    // TODO how to escape this text?
+    loggedInEmail.textContent = currentEmail;
+    loggedInPanel.hidden = false;
+    logoutBtn.onclick = Fxa.onLogout;
+    // TODO insert bug number tracking this feature
+    deleteAccountBtn.onclick = alert('delete not yet implemented');
+  }
+
+  function hideUnverifiedPanel() {
+    unverifiedPanel.hidden = true;
+    // TODO wire up other elements
+  }
+
+  function showUnverifiedPanel() {
+    unverifiedPanel.hidden = false;
+    // TODO wire up other elements
+  }
+
+  // TODO spinner also hides itself. come up with a better name.
+  function showSpinner() {
+    overlayPanel.hidden = false;
+    setTimeout(function() { overlayPanel.hidden = true }, 2000);
+  }
+
+  // TODO need to return anything else?
+  return {
+    init: init
+  };
+
+})();
+navigator.mozL10n.ready(FxaPanel.init());
