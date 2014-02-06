b65caac3a5dad4f6eeef2a7ad7769c7deb471406
Fernando Jiménez <ferjmoreno@gmail.com>
2014-02-06 12:36:19 +0100

    Bug 964390 - Hide fxa settings UI behind a pref

--- apps/settings/js/firefox_accounts/menu_loader.js
+++ apps/settings/js/firefox_accounts/menu_loader.js
@@ -6,6 +6,11 @@ navigator.mozL10n.ready(function loadWhenIdle() {
   var idleObserver = {
     time: 4,
     onidle: function() {
+      // don't load scripts if we have been preffed off
+      var displayPref = '[data-show-name="identity.fxaccounts.ui.enabled"]';
+      if (document.querySelector(displayPref).hidden) {
+        return;
+      }
       navigator.removeIdleObserver(idleObserver);
       LazyLoader.load([
         '/shared/js/fxa_iac_client.js',
